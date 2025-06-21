8cde38f102cd814a6fbb2dd1cb2d246e7852fadd
Frank Stallone <frank@stallone.dev>
2025-06-21 16:45:41 -0400

    content: small change on home page

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -2,12 +2,12 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function LandingPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 text-center">
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-50 sm:text-6xl">
-        Welcome to TENEX.AI
+        TENEX.AI
       </h1>
       <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
-        Analyze your logs to find anomalies and gain insights.
+        10× Your Threat Detection & Response with Next-Gen MDR
       </p>
       <div className="mt-10">
         <SignIn routing="hash" />
